d0723ca753d49ff7672b6f5029d95599c22326ba
ابولفضل محمدی <104293788+parzivalw00@users.noreply.github.com>
2022-05-25 04:32:46 +0430

    Add files via upload

---
+++ rgb.js
@@ -0,0 +1,71 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config.json');
+
+const size = config.colors;
+const rainbow = new Array(size);
+
+for (var i = 0; i < size; i++) {
+    var red = sin_to_hex(i, 0 * Math.PI * 2 / 3); // 0   deg
+    var blue = sin_to_hex(i, 1 * Math.PI * 2 / 3); // 120 deg
+    var green = sin_to_hex(i, 2 * Math.PI * 2 / 3); // 240 deg
+
+    rainbow[i] = '#' + red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+    var sin = Math.sin(Math.PI / size * 2 * i + phase);
+    var int = Math.floor(sin * 127) + 128;
+    var hex = int.toString(16);
+
+    return hex.length === 1 ? '0' + hex : hex;
+}
+
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+    for (let index = 0; index < servers.length; ++index) {
+        let server = bot.guilds.cache.get(servers[index]);
+        if (!server) {
+            if (config.logging) {
+                console.log(`[ColorChanger] Server ${servers[index]} was not found. Skipping.`);
+            }
+            continue;
+        }
+
+        let role = server.roles.cache.find(r => r.name === config.roleName);
+        if (!role) {
+            if (config.logging) {
+                console.log(`[ColorChanger] The role ${config.roleName} was not found on the server ${servers[index]}. Skipping.`);
+            }
+            continue;
+        }
+
+        role.setColor(rainbow[place]).catch(console.error);
+
+        if (config.logging) {
+            console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+        }
+    }
+
+    if (place == (size - 1)) {
+        place = 0;
+    } else {
+        place++;
+    }
+}
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.username}!`);
+    bot.user.setActivity("Server Security", {type: 'WATCHING'})
+    if (config.speed < 60000) {
+        console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned");
+        process.exit(1);
+    }
+    setInterval(changeColor, config.speed);
+    changeColor();
+});
+
+
+bot.login(config.token);
